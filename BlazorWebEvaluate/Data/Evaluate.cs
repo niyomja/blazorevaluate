41e9b9d47b508d7388d3915d8aaac73c1c31c546
Niyom Chamaree <niyomja@hotmail.com>
2020-08-19 12:59:03 +0700

    Update #2

--- BlazorWebEvaluate/Data/Evaluate.cs
+++ BlazorWebEvaluate/Data/Evaluate.cs
@@ -6,6 +6,35 @@ namespace BlazorWebEvaluate.Data
         public string full_name { get; set; }
         public string level { get; set; }
 
-        public BusinessAcu businessAcu { get; set; }
+        public int Max(){
+            int top = 0;
+            switch(level){
+                case "L1" : case "L2" : case "L3" : {
+                    top = 1;
+                };break;
+                case "L4" : case "L5" : {
+                    top = 2;
+                };break;
+                case "L6" : case "L7" : {
+                    top = 3;
+                };break;
+                case "L8" : case "L9" : {
+                    top = 4;
+                };break;
+                case "L10" : case "L11" : {
+                    top = 5;
+                };break;
+            }
+            return top;
+        }
+
+        public BusinessAcu businessAcu { get; set; } = new BusinessAcu();
+        public InitiativeData initiative { get; set; } = new InitiativeData();
+
+        public Collaboration collaboration { get; set; } = new Collaboration();
+
+        public ServiceExcellence serviceExcellence { get; set; } = new ServiceExcellence();
+        public SafetyAndSecurity safetyAndSecurity { get; set;} = new SafetyAndSecurity();
+        public DigitalLiteracy digitalLiteracy { get; set; } = new DigitalLiteracy();
     }
 }
--- BlazorWebEvaluate/Data/InitiativeData.cs
+++ BlazorWebEvaluate/Data/InitiativeData.cs
@@ -10,5 +10,31 @@ namespace BlazorWebEvaluate.Data
         public Level level1 { get; set; }
         public Level level3 { get; set; }
         public Level level5 { get; set; }
+        public int Scroll(){
+            int top = 0;
+
+            // L1 3
+            if(this.level1 !=null && this.level1.c1 == true && this.level1.c2 == true && this.level1.c3 == true){
+                top = 1;
+
+                // L3 5
+                if(this.level3 !=null && this.level3.c1 == true && this.level3.c2 == true && this.level3.c3 == true && this.level3.c4 == true && this.level3.c5 == true){
+                    top = 3;
+
+                    // L5 3
+                    if(this.level5 !=null && this.level5.c1 == true && this.level5.c2 == true && this.level5.c3 == true){
+                        top = 5;
+                    }else{
+                        top = 4;
+                    }
+                }else{
+                    top = 2;
+                }
+            }else{
+                top = 1;
+            }
+            
+            return top;
+         }
     }
 }
