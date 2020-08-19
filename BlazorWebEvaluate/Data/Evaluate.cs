9a4ff9e574946d2ad9ac468ddb771cb0cad83d44
Niyom Chamaree <niyomja@hotmail.com>
2020-08-19 10:26:09 +0700

    Update Add Server App

---
+++ BlazorWebEvaluate/Data/Evaluate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorWebEvaluate.Data
+{
+    public class Evaluate
+    {
+        public string full_name { get; set; }
+        public string level { get; set; }
+
+        public BusinessAcu businessAcu { get; set; }
+    }
+}
